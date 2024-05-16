c4da883c54bd24769278910583d1adbe574fd411
LM-Inocentes <louisemartininocentes@gmail.com>
2024-05-16 21:58:09 +0800

    Cleaned code

--- WildVote/src/app/views/dashboard/dashboard.component.ts
+++ WildVote/src/app/views/dashboard/dashboard.component.ts
@@ -4,20 +4,6 @@ import { map } from 'rxjs/operators';
 import { VoteService } from 'src/app/services/vote.service';
 import { Election } from 'src/app/shared/models/Election';
 
-interface IUser {
-  name: string;
-  state: string;
-  registered: string;
-  country: string;
-  usage: number;
-  period: string;
-  payment: string;
-  activity: string;
-  avatar: string;
-  status: string;
-  color: string;
-}
-
 @Component({
   templateUrl: 'dashboard.component.html',
   styleUrls: ['dashboard.component.scss']
