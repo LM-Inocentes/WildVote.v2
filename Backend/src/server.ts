c11e9db636ac3b2f49a5113b2dbec075349ccaa4
LM-Inocentes <louisemartininocentes@gmail.com>
2024-05-16 18:28:54 +0800

    Update server.ts

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -15,16 +15,31 @@ dbConnect();
 const app = express();
 
 
-app.use(express.json());
 app.use(cors({
-    origin: "https://wildvote.netlify.app",
+    origin: ["https://wildvote.netlify.app", "http://localhost:4200"],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
+app.use(express.json());
+
+app.options('*', cors());
+
+
 app.use("/api/users/", userRouter);
 app.use("/api/candidates/", candidateRouter);
 app.use("/api/misc/", miscRouter);
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://wildvote.netlify.app");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
